c67e2ae0bc982dce8403ade0c2ef9c23b1b6ee73
cfrancisco726 <cfrancisco726@gmail.com>
2018-01-18 23:53:40 -0500

    web app

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 // same as const React = require('react') ..non es6
 import { render } from 'react-dom';
 
-const App = () => (
-  <div className='app'>
-    <div className='landing'>
-      <h1>svideo</h1>
-      <input type='text' placeholder='Search' />
-      <a>or Browse all</a>
+// const App = () => (
+//   <div className='app'>
+//     <div className='landing'>
+//       <h1>svideo</h1>
+//       <input type='text' placeholder='Search' />
+//       <a>or Browse all</a>
+//     </div>
+//   </div>
+// );
+//
+// render(<App />, document.getElementById('app'));
+
+const App = () => {
+  return ( //if one line then implicit return otherwise you would need return {}
+    <div className='app'>
+      <div className='landing'>
+        <h1>svideo</h1>
+        <input type='text' placeholder='Search' />
+        <a>or Browse All</a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 render(<App />, document.getElementById('app'));
