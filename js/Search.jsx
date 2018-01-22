5ff48f46526f9c8188dcbcb2ea2c3cefb67b0605
cfrancisco726 <cfrancisco726@gmail.com>
2018-01-22 14:56:11 -0500

    props

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,8 +3,10 @@ import ShowCard from './ShowCard';
 import preload from '../data.json';
 
 const Search = () => (
-  <div className="search">
-    {preload.shows.map(show => <ShowCard show={show} />)}
+  <div className='search'>
+    { /* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> */ }
+    {preload.shows.map( show => <ShowCard key={show.imdbID} {...show} />)}
+
   </div>
 );
 
